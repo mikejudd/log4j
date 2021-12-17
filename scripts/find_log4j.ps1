377dc8fa65f743cc1b5f3997ae6cba9737548fa7
Mike Judd <mikejudd@users.noreply.github.com>
2021-12-17 11:11:34 -0500

    Create find_log4j.ps1

---
+++ scripts/find_log4j.ps1
@@ -0,0 +1,20 @@
+## Kudos to Matt Dewa4t in MSPGeek Slack
+
+$drives = Get-PSDrive -PSProvider FileSystem |Where-Object { $_.free -gt 0 } | Select-Object -ExpandProperty root
+$detected = @()
+foreach ($drive in $drives) {
+    $robocopyexitcode = (Start-Process robocopy  -ArgumentList "$drive c:\DOESNOTEXIST *.jar /S /XJ /L /FP /NS /NC /NDL /NJH /NJS /r:0 /w:0 /LOG:$env:temp\log4jfilescan.csv" -Wait).exitcode
+    if ($? -eq $True) {
+        $log4jfilescan = Import-Csv "$env:temp\log4jfilescan.csv" -Header FilePath -Delimiter '?'   
+        $detected += $log4jfilescan
+        Remove-Item "$env:temp\log4jfilescan.csv"
+    }
+}
+If ($detected) {
+    $vulnerablefiles = $detected | ForEach-Object { Select-String "JndiLookup.class" $_.FilePath } | Select-Object -exp Path | Sort-Object -Unique 
+}
+If ($vulnerablefiles) {
+    Write-Output "Detected vulnerable files: $vulnerablefiles" 
+}else {
+    Write-Output "No vulnerable files detected."
+}
