7d6bae5ae4c24ec09c9940e1f3f5928601fb31b0
Mike Judd <mikejudd@users.noreply.github.com>
2021-12-20 10:14:30 -0500

    added *.war & *.ear to files to scan for

--- scripts/find_log4j.ps1
+++ scripts/find_log4j.ps1
@@ -3,7 +3,7 @@
 $drives = Get-PSDrive -PSProvider FileSystem |Where-Object { $_.free -gt 0 } | Select-Object -ExpandProperty root
 $detected = @()
 foreach ($drive in $drives) {
-    $robocopyexitcode = (Start-Process robocopy  -ArgumentList "$drive c:\DOESNOTEXIST *.jar /S /XJ /L /FP /NS /NC /NDL /NJH /NJS /r:0 /w:0 /LOG:$env:temp\log4jfilescan.csv" -Wait).exitcode
+    $robocopyexitcode = (Start-Process robocopy  -ArgumentList "$drive c:\DOESNOTEXIST *.jar *.war *.ear /S /XJ /L /FP /NS /NC /NDL /NJH /NJS /r:0 /w:0 /LOG:$env:temp\log4jfilescan.csv" -Wait).exitcode
     if ($? -eq $True) {
         $log4jfilescan = Import-Csv "$env:temp\log4jfilescan.csv" -Header FilePath -Delimiter '?'   
         $detected += $log4jfilescan
